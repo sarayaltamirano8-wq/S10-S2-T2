695bcdc26dd531b234ce4da0d4af9bee8d8479ed
Emanuel <1485946@senati.pe>
2026-04-22 16:01:18 -0500

    fixes

--- index.js
+++ index.js
@@ -9,42 +9,44 @@ const db = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'inventario'
-})
+});
 
-app.post('/producto', (req,res)=>{
-    const {nombre, precio,stock} =req.body;
-    const query = 'INSERT INTO inventario (nombre, precio,stock) VALUES(?,?,?,?,?)';
-    db.query(query,[nombre, precio,stock], (err,result)=>{
-        if(err) throw err;
-        res.send('Estudiante creado con ID' + result.insertId);
+
+app.post('/producto', (req, res) => {
+    const { nombre, precio, stock } = req.body;
+    const query = 'INSERT INTO inventario (nombre, precio, stock) VALUES (?, ?, ?)';
+    db.query(query, [nombre, precio, stock], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.send('Producto creado con ID: ' + result.insertId);
     });
 });
 
-app.get('/producto',(req,res)=>{
-    db.query('SELECT * FROM inventario', (err, result)=>{
-        if(err) throw err;
+
+app.get('/producto', (req, res) => {
+    db.query('SELECT * FROM inventario', (err, result) => {
+        if (err) return res.status(500).send(err);
         res.json(result);
-    })
-})
+    });
+});
 
 
 app.put('/producto/:id', (req, res) => {
-    const { nombre, precio,stock } = req.body;
+    const { nombre, precio, stock } = req.body;
     const { id } = req.params;
     const query = 'UPDATE inventario SET nombre = ?, precio = ?, stock = ? WHERE id = ?';
-    db.query(query, [nombre, precio,stock, id], (err, result) => {
+    db.query(query, [nombre, precio, stock, id], (err, result) => {
         if (err) return res.status(500).send(err);
-        res.send('Estudiante actualizado correctamente');
+        res.send('Producto actualizado correctamente');
     });
 });
 
 
-app.delete('/producto/:id',(req,res)=>{
-    const {id} = res.params;
-    db.query('DELETE FROM inventario WHERE id = ?',[id],(err,result)=>{
-        if(err)throw err;
-        res.send('estudiante eliminado')
-    })
-})
+app.delete('/producto/:id', (req, res) => {
+    const { id } = req.params;
+    db.query('DELETE FROM inventario WHERE id = ?', [id], (err, result) => {
+        if (err) return res.status(500).send(err);
+        res.send('Producto eliminado');
+    });
+});
 
-app.listen(3000,()=> console.log('Servidor corriendo en puerto 3000'))
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
